2498a0a268f7af2acc935c8f39945a68e7a3bb56
proffix4 <proffix4@gmail.com>
2021-04-28 15:28:14 +0600

    windowActivated

--- src/kz/talipovsn/ij/multiforms/Form2.java
+++ src/kz/talipovsn/ij/multiforms/Form2.java
@@ -2,8 +2,6 @@ package kz.talipovsn.ij.multiforms;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 
 // Форма 2
